b31c745e5ce357e55d87b721c208a4801275b59d
Maxim <119421564+Radioacti1ve@users.noreply.github.com>
2023-09-21 18:37:11 +0300

    update

---
+++ test1.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "func.h"
+
+TEST(test_01, basic_test_set) {
+    ASSERT_TRUE()
+}
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
