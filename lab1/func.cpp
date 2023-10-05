042486b1adbcd656adde7a71369904cc306c07f3
Maxim <119421564+Radioacti1ve@users.noreply.github.com>
2023-10-05 18:47:59 +0300

    add lab1

---
+++ lab1/func.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include "func.h"
+
+std::string func(int n, char old_value, char new_value, std::string message) {
+    if(n <= 0)
+        return message;
+    int size_message = message.size();
+    for(int i = n-1; i < size_message; i+=n) {
+        if(message[i] == old_value) {
+            message[i] = new_value;
+        }
+    }
+    return message;
+}
